046ba26cb9239dd91217be1ca864fde5d50dd32e
willa <willaevangelista.pro@gmail.com>
2026-08-01 19:11:55 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int columns, int rows) {
+        this.columns = columns;
+        this.rows = rows;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+}
