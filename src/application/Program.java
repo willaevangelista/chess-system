f95ab36e1e640e28165ee3574761cd1456ae870c
willa <willaevangelista.pro@gmail.com>
2026-08-01 18:50:32 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
     }
 }
